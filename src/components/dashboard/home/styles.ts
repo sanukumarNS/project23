c7636df3085b2e4de4f78156e131c877c287b710
Sanu Kumar <sanu23kumar@gmail.com>
2023-03-29 11:35:20 +0530

    feat: home style

---
+++ src/components/dashboard/home/styles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  postParent: {
+    margin: 20,
+    padding: 20,
+  },
+  postTitle: {
+    fontSize: 20,
+  },
+  postBody: {
+    fontSize: 14,
+  },
+});
+
+export default styles;
